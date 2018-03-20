3b9f6d3f6bbcca8bd9a0e68975e025c2423da746
Rjmorcast <32773693+Rjmorcast@users.noreply.github.com>
2018-03-19 21:50:20 -0600

    traduce el mensaje

--- HojaTrabajo7/src/hojatrabajo7/HojaTrabajo7.java
+++ HojaTrabajo7/src/hojatrabajo7/HojaTrabajo7.java
@@ -8,6 +8,7 @@ package hojatrabajo7;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 /**
  *
@@ -20,6 +21,7 @@ public class HojaTrabajo7 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        ArrayList<String> mensajeTraducido = new ArrayList<String>();
         File f;
         FileReader fr;
 	BufferedReader br;
@@ -31,18 +33,47 @@ public class HojaTrabajo7 {
            String linea;
            while((linea = br.readLine())!= null){
               linea = linea.replaceAll("[()]", ""); //Para quitar los parentesis de la linea
-              System.out.println(linea);
               String[] datos = linea.split(","); //Para poder separar las palabras
-              System.out.println(datos[0]);
-              System.out.println(datos[1]);
               arbol.insertar(new Asociacion<String, String>(datos[0],datos[1]));
            }
+           br.close();
+           fr.close();
+           f = new File("./texto.txt");
+           fr = new FileReader(f);
+           br = new BufferedReader(fr);
+           while((linea = br.readLine())!= null){
+              linea = linea.replaceAll("[()]", ""); //Para quitar los parentesis de la linea
+              System.out.println(linea);
+              String word = "";
+              for (int i=0; i<linea.length();i++){
+                  if ((!" ".equals(linea.substring(i, i+1))) 
+                          && (!".".equals(linea.substring(i, i+1)))){
+                    word = word + linea.substring(i, i+1); 
+                  } else {
+                      Asociacion<String,String> result = arbol.buscar(new Asociacion(word, ""));
+                      if (result != null){
+                        mensajeTraducido.add(result.value);
+                      } else{
+                        mensajeTraducido.add("**"+word+"**");
+                      }
+                      
+                      word ="";
+                  }
+              }
+               
+           }
+           linea ="";
+           for (int i=0;i<mensajeTraducido.size();i++){
+               linea = linea +" "+mensajeTraducido.get(i);
+               
+           }
+           System.out.println(linea);
            br.close();
            fr.close();
         }catch(Exception e){
             System.err.println("Se produjo un error");
         }
-       arbol.mostrarInorder(arbol.getRaiz());
+       arbol.mostrarInorder(arbol.getRaiz()); 
        System.out.println("\n");
         
     }
